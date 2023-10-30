c95f3b95ba3d1b876b998054d2f6db8cc2dcb1b3
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-10-30 23:59:07 +0400

    add signing

---
+++ src/cryptography/shinkai_signing.ts
@@ -0,0 +1,128 @@
+import * as ed from "noble-ed25519";
+import { blake3 } from "@noble/hashes/blake3";
+import { ShinkaiMessage } from "../shinkai_message/shinkai_message";
+
+// TODO(Nico): move somewhere
+class ShinkaiMessageError extends Error {
+  constructor(message?: string) {
+    super(message);
+    this.name = "ShinkaiMessageError";
+  }
+}
+
+export async function verify_outer_layer_signature(
+  publicKey: Uint8Array,
+  shinkaiMessage: ShinkaiMessage
+): Promise<boolean> {
+  try {
+    const hexSignature = shinkaiMessage.external_metadata.signature;
+    console.log("Signature from message:", hexSignature);
+    if (!hexSignature) {
+      throw new ShinkaiMessageError(`Signature is missing`);
+    }
+    const matched = hexSignature.match(/.{1,2}/g);
+    if (!matched) {
+      throw new ShinkaiMessageError(`Invalid signature format`);
+    }
+    const signatureBytes = new Uint8Array(
+      matched.map((byte) => parseInt(byte, 16))
+    );
+
+    // Create a copy of the message with an empty signature
+    let messageCopy = JSON.parse(JSON.stringify(shinkaiMessage));
+    messageCopy.external_metadata.signature = "";
+
+    const sortedShinkaiMessage = sortObjectKeys(shinkaiMessage);
+    // Calculate the hash of the modified message
+    const messageHash = blake3FromObj(sortedShinkaiMessage);
+    const messageHashMatched = messageHash.match(/.{1,2}/g);
+    if (!messageHashMatched) {
+      throw new ShinkaiMessageError(`Invalid message hash format`);
+    }
+    const messageHashBytes = new Uint8Array(
+      messageHashMatched.map((byte) => parseInt(byte, 16))
+    );
+
+    return await ed.verify(signatureBytes, messageHashBytes, publicKey);
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new ShinkaiMessageError(`Signing error: ${e.message}`);
+    } else {
+      throw new ShinkaiMessageError(`Signing error: ${e}`);
+    }
+  }
+}
+
+export async function sign_outer_layer(
+  secretKey: Uint8Array,
+  shinkaiMessage: ShinkaiMessage
+): Promise<{ signature: string }> {
+  try {
+    // Ensure that external_metadata.signature is empty
+    let messageCopy = JSON.parse(JSON.stringify(shinkaiMessage));
+    messageCopy.external_metadata.signature = "";
+
+    const sortedShinkaiMessage = sortObjectKeys(shinkaiMessage);
+    const messageHash = blake3FromObj(sortedShinkaiMessage);
+    const messageHashMatched = messageHash.match(/.{1,2}/g);
+    if (!messageHashMatched) {
+      throw new ShinkaiMessageError(`Invalid message hash format`);
+    }
+    const messageHashBytes = new Uint8Array(
+      messageHashMatched.map((byte) => parseInt(byte, 16))
+    );
+
+    const signature = await ed.sign(messageHashBytes, secretKey);
+    shinkaiMessage.external_metadata.signature = Array.from(signature)
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+
+    return shinkaiMessage.external_metadata;
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new ShinkaiMessageError(`Signing error: ${e.message}`);
+    } else {
+      throw new ShinkaiMessageError(`Signing error: ${e}`);
+    }
+  }
+}
+
+function blake3FromObj(obj: any): string {
+  let sortedString = JSON.stringify(obj, Object.keys(obj).sort());
+  let hashAlt = blake3(sortedString);
+  let hashAltHex = Array.from(new Uint8Array(hashAlt))
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+  return hashAltHex;
+}
+
+function sortObjectKeys(obj: ShinkaiMessage): object {
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(sortObjectKeys);
+  }
+
+  const sortedObj: { [key: string]: any } = {};
+
+  Object.keys(obj)
+    .sort()
+    .forEach((key: string) => {
+      if (
+        key in obj &&
+        typeof obj[key as keyof ShinkaiMessage] === "object" &&
+        obj[key as keyof ShinkaiMessage] !== null
+      ) {
+        sortedObj[key] =
+          obj[key as keyof ShinkaiMessage] instanceof Object
+            ? sortObjectKeys(obj[key as keyof ShinkaiMessage] as any)
+            : obj[key as keyof ShinkaiMessage];
+      } else {
+        sortedObj[key] = obj[key as keyof ShinkaiMessage];
+      }
+    });
+
+  return sortedObj;
+}
--- tests/crypto.test.ts
+++ tests/crypto.test.ts
@@ -6,9 +6,15 @@ import {
   encryptMessage,
   encryptMessageWithPassphrase,
   generateEncryptionKeys,
+  generateSignatureKeys,
 } from "../src/cryptography/shinkai_encryption";
 import { Crypto } from "@peculiar/webcrypto";
 import sodium from "libsodium-wrappers-sumo";
+import { ShinkaiMessage } from "../src/shinkai_message/shinkai_message";
+import {
+  sign_outer_layer,
+  verify_outer_layer_signature,
+} from "../src/cryptography/shinkai_signing";
 
 const crypto = new Crypto();
 globalThis.crypto = crypto;
@@ -67,5 +73,62 @@ describe("Cryptography Functions", () => {
 
     // The decrypted message should be the same as the original message
     expect(decryptedMessage).toBe(originalMessage);
+  });
+
+  test("sign and verify outer layer signature", async () => {
+    const unsorted_messageJson = `{
+        "body": {
+            "unencrypted": {
+                "message_data": {
+                    "unencrypted": {
+                        "message_content_schema": "TextContent",
+                        "message_raw_content": "hey!"
+                    }
+                },
+                "internal_metadata": {
+                    "inbox": "inbox::@@node1.shinkai/main/device/main_device::@@node2.shinkai::false",
+                    "sender_subidentity": "main/device/main_device",
+                    "encryption": "None",
+                    "recipient_subidentity": "",
+                    "signature": "c6d0115c0878fbf2279f98aab67c0e9cb1af63825f49dca48d6e4420eba0ceb973e00488ba0905c9afd09254f0dac48c468fdcb1d6c5ab5ca4c5dd70a440b903"
+                }
+                
+            }
+        },
+        "external_metadata": {
+            "recipient": "@@node2.shinkai",
+            "other": "",
+            "sender": "@@node1.shinkai",
+            "scheduled_time": "2023-08-25T22:44:01.132Z",
+            "intra_sender": "intra_sender",
+            "signature": "d7d0115c0878fbf2279f98aab67c0e9cb1af63825f49dca48d6e4420eba0ceb973e00488ba0905c9afd09254f0dac48c468fdcb1d6c5ab5ca4c5dd70a389f123"
+        },
+        "encryption": "DiffieHellmanChaChaPoly1305",
+        "version": "V1_0"
+      }`;
+
+    const keys = await generateSignatureKeys();
+
+    // Convert keys from HexString to Uint8Array
+    const privateKey = hexToBytes(keys.my_identity_sk_string);
+    const publicKey = hexToBytes(keys.my_identity_pk_string);
+
+    // Parse the message JSON to a ShinkaiMessage object
+    const shinkaiMessage: ShinkaiMessage = JSON.parse(unsorted_messageJson);
+
+    // Sign the message
+    const signedMessage = await sign_outer_layer(privateKey, shinkaiMessage);
+
+    // Overwrite the signature in shinkaiMessage with the one from signedMessage
+    shinkaiMessage.external_metadata.signature = signedMessage.signature;
+
+    // Verify the signature
+    const isValid = await verify_outer_layer_signature(
+      publicKey,
+      shinkaiMessage
+    );
+
+    // The signature should be valid
+    expect(isValid).toBe(true);
   });
 });
