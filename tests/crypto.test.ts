15dfb016f1adb0c18738f73ffc67f8542757bc68
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-01-26 22:35:37 -0600

    makes it clear

--- tests/crypto.test.ts
+++ tests/crypto.test.ts
@@ -289,9 +289,9 @@ describe("Cryptography Functions", () => {
             "sender": "@@node1.shinkai",
             "scheduled_time": "2023-08-25T22:44:01.132Z",
             "intra_sender": "intra_sender",
-            "signature": "d7d0115c0878fbf2279f98aab67c0e9cb1af63825f49dca48d6e4420eba0ceb973e00488ba0905c9afd09254f0dac48c468fdcb1d6c5ab5ca4c5dd70a389f123"
+            "signature": ""
         },
-        "encryption": "DiffieHellmanChaChaPoly1305",
+        "encryption": "None",
         "version": "V1_0"
       }`;
 
