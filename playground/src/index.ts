566b039a1016586e5529faa17962914d1973a25f
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-01-28 23:01:02 -0600

    get messages from inbox

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,23 +1,25 @@
 import { ShinkaiManager } from "./shinkai_manager";
 import { postData } from "./utils";
 import {
+  InboxName,
   MessageSchemaType,
   ShinkaiMessage,
   ShinkaiMessageBuilder,
   TSEncryptionMethod,
 } from "@shinkai_protocol/shinkai-typescript-lib";
 
-async function sendMessage(shinkai_manager: ShinkaiManager, content: string, job_id: string) {
-  const message_job = await shinkai_manager.buildJobMessage(
-    content,
-    job_id
-  );
+async function sendMessage(
+  shinkai_manager: ShinkaiManager,
+  content: string,
+  job_id: string
+) {
+  const message_job = await shinkai_manager.buildJobMessage(content, job_id);
 
   let resp = await postData(message_job, "/v1/job_message");
   console.log("### Response:");
   console.log(resp);
 
-  if (resp.status === 'success') {
+  if (resp.status === "success") {
     return resp.data;
   } else {
     throw new Error(`Job creation failed with status: ${resp.status}`);
@@ -25,7 +27,9 @@ async function sendMessage(shinkai_manager: ShinkaiManager, content: string, job
 }
 
 async function getMessages(shinkai_manager: ShinkaiManager, inbox: string) {
-
+  const message = await shinkai_manager.buildGetMessagesForInbox(inbox);
+  let resp = await postData(message, "/v1/last_messages_from_inbox");
+  console.log(JSON.stringify(resp, null, 2));
 }
 
 async function createJob(shinkai_manager: ShinkaiManager, agent: string) {
@@ -37,7 +41,7 @@ async function createJob(shinkai_manager: ShinkaiManager, agent: string) {
   console.log("### Response:");
   console.log(resp);
 
-  if (resp.status === 'success') {
+  if (resp.status === "success") {
     return resp.data;
   } else {
     throw new Error(`Job creation failed with status: ${resp.status}`);
@@ -52,8 +56,10 @@ async function getInboxes(shinkai_manager: ShinkaiManager) {
 
 async function main() {
   // Shinkai Node1
-  const encryption_sk: string = "88b49468ed3ee4ea079f75eef9f651f09d3f18fd3a575c3c48d0052347462179";
-  const signature_sk: string = "91adf9c548e3ea0ba3f3fa38ecd239c3bec0dc5a63dcb430746ec4c43160d97e";
+  const encryption_sk: string =
+    "88b49468ed3ee4ea079f75eef9f651f09d3f18fd3a575c3c48d0052347462179";
+  const signature_sk: string =
+    "91adf9c548e3ea0ba3f3fa38ecd239c3bec0dc5a63dcb430746ec4c43160d97e";
   const receiver_pk =
     "798cbd64d78c4a0fba338b2a6349634940dc4e5b601db1029e02c41e0fe05679";
   const profile_name = "main";
@@ -69,12 +75,17 @@ async function main() {
     device_name
   );
 
-  let job_id = await createJob(shinkai_manager, "main/agent/my_gpt");
-  console.log("### Job ID:", job_id);
+  let job_id = InboxName.getJobInboxNameFromParams("jobid_98da764e-a816-48d5-af02-0850a618930f").value;
+  let resp = await getMessages(
+    shinkai_manager,
+    job_id
+  );
 
-  let answer = await sendMessage(shinkai_manager, "Hello World! how are you?", job_id);
-  console.log("### Answer:", answer);
+  // let job_id = await createJob(shinkai_manager, "main/agent/my_gpt");
+  // console.log("### Job ID:", job_id);
 
+  // let answer = await sendMessage(shinkai_manager, "Hello World! how are you?", job_id);
+  // console.log("### Answer:", answer);
 
   // await getInboxes(shinkai_manager);
   // console.log(resp);
